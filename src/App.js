b6ddca5bad6f219f06dd52b6058f731852f049b5
abayo-luc <luc.bayo@gmail.com>
2020-02-25 17:22:04 +0200

    :sparkles: add home page
    - Add nav links
    - Add background image
    - Choose background color
    - Configures sass
    
    Luc :green_heart:

--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import HomePage from './pages/Home';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-       <h1>Luc</h1>
-      </header>
-    </div>
-  );
+	return (
+		<div className='app'>
+			<HomePage />
+		</div>
+	);
 }
 
 export default App;
---
+++ src/pages/Home/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import HeaderComponent from '../../components/Header';
+import './styles.scss'
+const HomePage = () => (
+	<div className='home-page'>
+		<HeaderComponent />
+	</div>
+);
+
+export default HomePage;
